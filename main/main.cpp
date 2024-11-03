83e50a26970765704f135dbd2b122757fdddbb8f
SuphawadiP <65030269@kmitl.ac.th>
2024-11-03 23:08:28 +0700

    update counter

--- main/main.cpp
+++ main/main.cpp
@@ -6,17 +6,22 @@ SevenSegment s2(4) ;
 
 extern "C" void app_main(void)
 {
+
+    uint8_t counter = 0;
     while(1)
     {
-        s1.DisplayNumber(1);
+        s1.DisplayNumber(counter / 10);
         s1.DisplayOn();
-        vTaskDelay(10/portTICK_PERIOD_MS);
+        vTaskDelay(100/portTICK_PERIOD_MS);
         s1.DisplayOff();
 
-        s2.DisplayNumber(5);
+        s2.DisplayNumber(counter % 10);
         s2.DisplayOn();
-        vTaskDelay(10/portTICK_PERIOD_MS);
+        vTaskDelay(100/portTICK_PERIOD_MS);
         s2.DisplayOff();
+        counter++;
+        if (counter > 99)
+            counter = 0;
 
         // for (int i = 0; i < 9; i++)
         // {
