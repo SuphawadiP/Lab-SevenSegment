7db4110684a0e33eee3bb93766e431184aae8346
SuphawadiP <65030269@kmitl.ac.th>
2024-11-03 23:19:32 +0700

    modify tsklDLE_PRIORITY to 10

--- main/main.cpp
+++ main/main.cpp
@@ -1,27 +1,71 @@
 #include <stdio.h>
 #include "SevenSegment.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-SevenSegment s1(0) ;
-SevenSegment s2(4) ;
+SevenSegment s1(0);
+SevenSegment s2(4);
 
-extern "C" void app_main(void)
+// create counter as global variable
+uint8_t counter = 0;
+
+// create task handle for LED display task
+TaskHandle_t xSevenSegmentHandle = NULL;
+
+// create task and move code into it
+void vTaskScanSevenSegment(void *Parameters)
 {
-
-    uint8_t counter = 0;
-    while(1)
+    while (1)
     {
         s1.DisplayNumber(counter / 10);
         s1.DisplayOn();
-        vTaskDelay(100/portTICK_PERIOD_MS);
+        vTaskDelay(10 / portTICK_PERIOD_MS);
         s1.DisplayOff();
 
         s2.DisplayNumber(counter % 10);
         s2.DisplayOn();
-        vTaskDelay(100/portTICK_PERIOD_MS);
+        vTaskDelay(10 / portTICK_PERIOD_MS);
         s2.DisplayOff();
-        counter++;
-        if (counter > 99)
-            counter = 0;
+    }
+}
+
+
+// in main, create display task
+extern "C" void app_main(void)
+{
+    xTaskCreate(vTaskScanSevenSegment, 
+      "Seven Seg", 1024, NULL, 
+      10, &xSevenSegmentHandle);
+}
+
+
+
+
+
+// #include <stdio.h>
+// #include "SevenSegment.h"
+
+// SevenSegment s1(0) ;
+// SevenSegment s2(4) ;
+
+// extern "C" void app_main(void)
+// {
+
+//     uint8_t counter = 0;
+//     while(1)
+//     {
+//         s1.DisplayNumber(counter / 10);
+//         s1.DisplayOn();
+//         vTaskDelay(100/portTICK_PERIOD_MS);
+//         s1.DisplayOff();
+
+//         s2.DisplayNumber(counter % 10);
+//         s2.DisplayOn();
+//         vTaskDelay(100/portTICK_PERIOD_MS);
+//         s2.DisplayOff();
+//         counter++;
+//         if (counter > 99)
+//             counter = 0;
 
         // for (int i = 0; i < 9; i++)
         // {
@@ -74,5 +118,5 @@ extern "C" void app_main(void)
         // s2.DisplayNum9();
         // vTaskDelay(500/portTICK_PERIOD_MS);
         // s2.DisplayBlank();
-    } 
-}
+//     } 
+// }
